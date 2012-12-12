8fef02ba024a2c3dcbbe7fa2081994391ef1542c
gpittau <gpittau@gmail.com>
2012-12-12 15:49:12 -0300

    Add associations syntax

--- tools/json2hal.js
+++ tools/json2hal.js
@@ -85,7 +85,7 @@ ensureDir(
 		)
 
 	var	transformers
-	=	make_transformers(_,hal_builder,collection_builder)({find:store_find,filter:store_filter},transforms,hal)
+	=	make_transformers(_,hal_builder,collection_builder,uritemplate)({find:store_find,filter:store_filter},transforms,hal)
 
 		_(sources)
 		.each(
